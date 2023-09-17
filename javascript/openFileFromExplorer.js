609cba9fe07df0a91ff59ae41eb43ca626ca9bcd
jshjelse <jshjelse@stud.ntnu.no>
2023-09-17 13:55:27 +0200

    Tester på nytt

--- javascript/openFileFromExplorer.js
+++ javascript/openFileFromExplorer.js
@@ -1,16 +1,6 @@
-const clickOpen = document.getElementById("clickOpen"); // Knappen vi gir funksjonalitet
-const dialog = document.getElementById("dialog"); // Input-feltet vi har skjult, men som lagrer dataen som velges
+const clickOpen = document.getElementById("onClick"); // Knappen vi gir funksjonalitet
+const dialog = document.getElementById("fileSelect"); // Input-feltet vi har skjult, men som lagrer dataen som velges
 
 function clickMe() {
     dialog.click();
 }
-
-dialog.addEventListener("input", () => {
-    if (dialog.files.length) {
-        for (i = 0; i < dialog.files.length; i++) {
-            var file = shp(dialog.files[i].name);
-            L.geoJSON().addData(file).addTo(map);
-            document.getElementById("layers").innerHTML += "Hei!";
-        }
-    }
-});
