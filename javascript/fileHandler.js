c23d3bbb9943b9e58a4318b0ace0474c2b7b86be
jshjelse <jshjelse@stud.ntnu.no>
2023-09-18 21:32:16 +0200

    Fortsette så smått med å lese shapefiler

--- javascript/fileHandler.js
+++ javascript/fileHandler.js
@@ -1,14 +1,40 @@
-const input = document.getElementById("fileSelect");
-
-input.addEventListener("change", handleFile(), false);
-
 function handleFile() {
-    const fileList = file.files;
-    const fileName = fileList[0].name;
+  input = document.getElementById("fileInput");
+  if (!input.files[0]) {
+    alert("Please select a file before clicking 'Load'.")
+  }
+  else {
+    file = input.files[0];
+    fr = new FileReader();
 
-    document.getElementById("layers").innerHTML = fileName;
+    arrayBuffer = fr.readAsArrayBuffer(file);
 
-    //shp("files/pandr.zip").then(function(geojson) {
-		//see bellow for whats here this internally call shp.parseZip()
-	//});
+    //document.getElementById("layers").innerHTML = arrayBuffer.type;
+
+    var shpfile = new L.Shapefile(arrayBuffer, {
+			onEachFeature: function(feature, layer) {
+				if (feature.properties) {
+					layer.bindPopup(Object.keys(feature.properties).map(function(k) {
+						return k + ": " + feature.properties[k];
+					}).join("<br />"), {
+						maxHeight: 200
+					});
+				}
+			}
+		});
+
+		shpfile.addTo(map);
+		
+    shpfile.once("data:loaded", function() {
+			alert("Finished loaded shapefile 8)");
+		});
+
+    /*fr.onload = receiveBinary;
+    fr.readAsArrayBuffer(file);*/
+  }
+  function receiveBinary() {
+    result = fr.result;
+    var shpfile = new L.Shapefile(result);
+    shpfile.addTo(map);
+  }
 }
