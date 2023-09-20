4ebce383384644e45b5ac21d4d491736f929d34a
jshjelse <jshjelse@stud.ntnu.no>
2023-09-20 19:25:43 +0200

    Foreløpig ferdigstilling

--- javascript/geoJSON.js
+++ javascript/geoJSON.js
@@ -1,1 +1,18 @@
-L.GeoJSON.AJAX("exampleData/vann_2.geojson").addTo(map);
+var rectangle = [{
+    "type": "Feature",
+    "properties": {"name": "Gløshaugen"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [10.403787, 63.420031],
+            [10.410954, 63.414691],
+            [10.404863, 63.414188],
+            [10.398170, 63.418901],
+            [10.403787, 63.420031]
+        ]]
+    }
+}];
+
+var geoLayer = L.geoJSON().addTo(map);
+
+geoLayer.addData(rectangle);
