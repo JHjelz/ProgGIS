2e6169cc95e7ecd67671995f38112fb5e4ed9b4a
jshjelse <jshjelse@stud.ntnu.no>
2023-10-02 09:33:53 +0200

    Ferdigstille

--- javascript/difference.js
+++ javascript/difference.js
@@ -1,12 +1,19 @@
 function difference() {
     var input1 = document.getElementById("differenceSelect_1").value;
-    var layer1 = overlayMaps[input1];
+    var layer1 = overlayMaps[input1].toGeoJSON();
     var input2 = document.getElementById("differenceSelect_2").value;
-    var layer2 = overlayMaps[input2];
+    var layer2 = overlayMaps[input2].toGeoJSON();
     var name = document.getElementById("differenceName").value;
 
+    // Må konverteres fra feature collection til multipolygon:
+    var coords1 = featureCollectionToMultiPolygon(layer1);
+    var coords2 = featureCollectionToMultiPolygon(layer2);
+    
+    var multiPolygon1 = turf.multiPolygon(coords1);
+    var multiPolygon2 = turf.multiPolygon(coords2);
+
     try {
-        var difference = turf.difference(layer1.toGeoJSON(), layer2.toGeoJSON());
+        var difference = turf.difference(multiPolygon1, multiPolygon2);
         var newLayer = L.geoJSON(difference);
         
         overlayMaps[name] = newLayer;
