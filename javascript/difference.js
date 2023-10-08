82ccfe10b449566c02dc79085f1db5a09eefdca1
jshjelse <jshjelse@stud.ntnu.no>
2023-10-08 13:24:20 +0200

    Ferdig

--- javascript/difference.js
+++ javascript/difference.js
@@ -6,14 +6,16 @@ function difference() {
     var name = document.getElementById("differenceName").value;
 
     // Må konverteres fra feature collection til multipolygon:
-    var coords1 = featureCollectionToMultiPolygon(layer1);
-    var coords2 = featureCollectionToMultiPolygon(layer2);
-    
-    var multiPolygon1 = turf.multiPolygon(coords1);
-    var multiPolygon2 = turf.multiPolygon(coords2);
+    var multiPolygon1 = featureCollectionToMultiPolygon(layer1);
+    var multiPolygon2 = featureCollectionToMultiPolygon(layer2);
 
     try {
         var difference = turf.difference(multiPolygon1, multiPolygon2);
+        
+        if (isMultiPolygon(difference)) {
+            difference = multiPolygonToFeatureCollection(difference);
+        }
+        
         var newLayer = L.geoJSON(difference, {style: getStyle()});
         
         overlayMaps[name] = newLayer;
--- javascript/intersect.js
+++ javascript/intersect.js
@@ -6,14 +6,16 @@ function intersection() {
     var name = document.getElementById("intersectionName").value;
     
     // Må konverteres fra feature collection til multipolygon:
-    var coords1 = featureCollectionToMultiPolygon(layer1);
-    var coords2 = featureCollectionToMultiPolygon(layer2);
-    
-    var multiPolygon1 = turf.multiPolygon(coords1);
-    var multiPolygon2 = turf.multiPolygon(coords2);
+    var multiPolygon1 = featureCollectionToMultiPolygon(layer1);
+    var multiPolygon2 = featureCollectionToMultiPolygon(layer2);
     
     try {
         var intersection = turf.intersect(multiPolygon1, multiPolygon2);
+        
+        if (isMultiPolygon(intersection)) {
+            intersection = multiPolygonToFeatureCollection(intersection);
+        }
+
         var newLayer = new L.GeoJSON(intersection, {style: getStyle()});
         
         overlayMaps[name] = newLayer;
