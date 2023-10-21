a4a9d6f6e92fd44d201efd7476a07e30120fb977
jshjelse <jshjelse@stud.ntnu.no>
2023-10-21 21:01:17 +0200

    Ferdigstille

--- javascript/fileHandler.js
+++ javascript/fileHandler.js
@@ -7,22 +7,25 @@ function handleFile() {
         return alert("No chosen file!")
     }
 
-    console.log(selectedFile);
-    
-    var fr = new FileReader();
+    //console.log(selectedFile); // Printer bare selve filen til loggen
+
     fileHandler.value = "";
     document.getElementById("loadButton").style.backgroundColor = "orangered";
     
-    /*
-    fr.onload = function() {
-        var layer = omnivore.geojson(fr.result).addTo(map);
-        map.fitBounds(layer.getBounds())
+    var read = new FileReader();
+    read.readAsDataURL(selectedFile);
+    
+    var newLayer = L.geoJSON(null, {style: getStyle()})
+
+    read.onloadend = function() {
+        fetch(read.result).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            newLayer.addData(data);
+        })
     }
 
-    fr.readAsDataURL(selectedFile);
-
-    console.log("Kanskje bra...")
-    */
-
-    //console.log(data)
+    overlayMaps[selectedFile.name] = newLayer;
+    handleLayer(selectedFile.name);
+    updateSidebar();
 }
