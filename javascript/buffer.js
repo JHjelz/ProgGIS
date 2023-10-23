6c0e6eaa6d4b5bf64565d0d6fe283ca2e12f49bd
jshjelse <jshjelse@stud.ntnu.no>
2023-10-23 15:52:16 +0200

    BugFix

--- javascript/buffer.js
+++ javascript/buffer.js
@@ -37,6 +37,7 @@ function makeBuffer() {
         
         if (document.getElementById("bufferCheck").checked) { // Om en har huket av for at en skal 'dissolve' gjøres det
             // Dette er nytt
+            console.log(JSON.stringify(buffer));
             if (isMultiPolygon(buffer)) { // Kan ikke sende MultiPolygon inn i dissolved
                 buffer = multiPolygonToFeatureCollection(buffer);
             }
